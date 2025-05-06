890852192e09cada455f5d62983a207447cab079
furkanyigit34 <134547018+furkanyigit34@users.noreply.github.com>
2025-05-06 17:22:03 +0300

    Renamed qr.tsx to _qr.tsx to bypass build issue on Vercel

---
+++ pages/[slug]/_qr.tsx
@@ -0,0 +1,11 @@
+// Next.js bileşeni
+import React from 'react';
+
+export default function QRPage() {
+  return (
+    <div>
+      <h1>QR Kodu Sayfası</h1>
+      {/* Sayfa içeriği */}
+    </div>
+  );
+} 
